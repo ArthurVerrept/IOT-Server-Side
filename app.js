dcc70440121a96d356a2592527d438429be02837
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2019-04-04 17:20:58 +0100

    more testing

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); 
 
+//this waits for the
+app.listen(port, function(){
+    console.log(`app is listening on port ${port}`);
+});
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/www/"));
 
