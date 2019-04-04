3f04e753e432b8fe14771e61ea88cd8e91e03b22
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2019-04-04 17:16:34 +0100

    more testing

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app =  express();
 var port = process.env.PORT || 3000;
 
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); 
 
 app.listen(port, function(){
     console.log(`app is listening on port ${port}`);
